017acc6bbb02633842bccced9d5c53df9737b379
abhinand <abhinandma007@gmail.com>
2026-05-02 00:21:40 +0530

    fix env settings

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#r$wmwv=w33c01)@=75%=o^t2%buy@bg-b8%y^=ammpde_s=21'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 
+    'django-insecure-#r$wmwv=w33c01)@=75%=o^t2%buy@bg-b8%y^=ammpde_s=21'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
